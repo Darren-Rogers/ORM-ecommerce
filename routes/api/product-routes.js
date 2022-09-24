4fe93dea4b913e526413e7327b861ddf3fdadb12
Sreggie <darren.rogers06@gmail.com>
2022-09-24 17:05:37 -0400

    working through error message when seeding

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -119,8 +119,15 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy(req.body, {
+      where:{id:req.params.body}
+    })
+  } catch (err) {
+    res.json(err)
+  }
 });
 
 module.exports = router;
